fea1668d6100f5ca3062fd105c22cd865f2bfefe
Jorian Woltjer <jorianwoltjer@hotmail.com>
2022-12-18 14:59:34 +0100

    day18b

--- day18/src/bin/day18b.rs
+++ day18/src/bin/day18b.rs
@@ -1,3 +1,50 @@
+use day18::Shape;
+
 fn main() {
-    unimplemented!("day18b");
+    let input = include_str!("../../input.txt");
+
+    let shape: Shape = input.parse().unwrap();
+    let surface_area = shape.surface_area_without_gaps();
+
+    println!("Area: {surface_area}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn small_example() {
+        let input = "\
+1,1,1
+2,1,1";
+
+        let shape: Shape = input.parse().unwrap();
+        let surface_area = shape.surface_area_without_gaps();
+
+        assert_eq!(surface_area, 10);
+    }
+    
+    #[test]
+    fn larger_example() {
+        let input = "\
+2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5";
+
+        let shape: Shape = input.parse().unwrap();
+        let surface_area = shape.surface_area_without_gaps();
+
+        assert_eq!(surface_area, 58);
+    }
 }
--- day18/src/lib.rs
+++ day18/src/lib.rs
@@ -1,13 +1,16 @@
-use std::{str::FromStr, collections::HashSet};
+use std::{str::FromStr, collections::{HashSet, VecDeque}};
 
 #[derive(Debug)]
 pub struct Shape {
     cubes: HashSet<(isize, isize, isize)>,
+    min: (isize, isize, isize),
+    max: (isize, isize, isize),
 }
 impl FromStr for Shape {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // Parse string
         let cubes = s.lines().map(|line| {
             let mut s = line.split(",");
             (s.next().unwrap().parse().unwrap(), 
@@ -15,7 +18,19 @@ impl FromStr for Shape {
              s.next().unwrap().parse().unwrap())
         });
 
-        Ok(Self { cubes: HashSet::from_iter(cubes) })
+        // Calculate bounding box
+        let min = (
+            cubes.clone().map(|cube| cube.0).min().unwrap(),
+            cubes.clone().map(|cube| cube.1).min().unwrap(),
+            cubes.clone().map(|cube| cube.2).min().unwrap(),
+        );
+        let max = (
+            cubes.clone().map(|cube| cube.0).max().unwrap(),
+            cubes.clone().map(|cube| cube.1).max().unwrap(),
+            cubes.clone().map(|cube| cube.2).max().unwrap(),
+        );
+
+        Ok(Self { cubes: HashSet::from_iter(cubes), min, max })
     }
 }
 impl Shape {
@@ -32,6 +47,48 @@ impl Shape {
         }
 
         area
+    }
+
+    pub fn surface_area_without_gaps(&self) -> usize {
+        // Initial guess
+        let mut area = self.cubes.len() * 6;
+
+        for cube in &self.cubes {
+            for adjacent in get_neighbors(cube) {
+                if self.cubes.contains(&adjacent) || !self.path_to_outside(adjacent) {  // If another cube is blocking this path
+                    area -= 1;
+                }
+            }
+        }
+
+        area
+    }
+
+    /// Breadth first search to anywhere outside the bounding box
+    fn path_to_outside(&self, root: (isize, isize, isize)) -> bool {
+        let mut queue = VecDeque::from([root]);
+        let mut explored = HashSet::new();
+
+        while !queue.is_empty() {
+            let cube = queue.pop_front().unwrap();  // Get from queue
+
+            // If path lead to outside the bounding box
+            if  cube.0 < self.min.0 || cube.1 < self.min.1 || cube.2 < self.min.2 || 
+                cube.0 > self.max.0 || cube.1 > self.max.1 || cube.2 > self.max.2 {
+                
+                return true;
+            }
+
+            // Add next paths
+            for adjacent in get_neighbors(&cube) {
+                if !explored.contains(&adjacent) && !self.cubes.contains(&adjacent) {  // Ignore if already explored or is solid
+                    explored.insert(adjacent.clone());
+                    queue.push_back(adjacent.clone());
+                }
+            }
+        }
+
+        false // All paths ended
     }
 }
 
