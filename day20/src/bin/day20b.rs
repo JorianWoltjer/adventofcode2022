ef836aa5d907d2619f92f68b2e8d3eac3f98aef6
Jorian Woltjer <jorianwoltjer@hotmail.com>
2022-12-20 12:47:09 +0100

    day20b

--- day20/src/bin/day20b.rs
+++ day20/src/bin/day20b.rs
@@ -1,3 +1,44 @@
+use day20::move_by_value_times;
+
 fn main() {
-    unimplemented!("day20b");
+    let input = include_str!("../../input.txt");
+
+    let list: Vec<isize> = input.lines().map(|line| line.parse::<isize>().unwrap() * 811589153).collect();
+    let moved = move_by_value_times(list, 10);
+
+    let zero_index = moved.iter().position(|value| value == &0).unwrap();
+
+    let sum: isize = [1000, 2000, 3000].iter().map(|n| moved[(zero_index + n) % moved.len()]).sum();
+
+    println!("Sum: {}", sum);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input = "\
+1
+2
+-3
+3
+-2
+0
+4";
+
+        let list: Vec<isize> = input.lines().map(|line| line.parse::<isize>().unwrap() * 811589153).collect();
+        assert_eq!(list, vec![811589153, 1623178306, -2434767459, 2434767459, -1623178306, 0, 3246356612]);
+        let moved = move_by_value_times(list, 10);
+
+        assert_eq!(moved, vec![0, -2434767459, 1623178306, 3246356612, -1623178306, 2434767459, 811589153]);
+
+        let zero_index = moved.iter().position(|value| value == &0).unwrap();
+
+        let result: Vec<isize> = [1000, 2000, 3000].iter().map(|n| moved[(zero_index + n) % moved.len()]).collect();
+        assert_eq!(result, vec![811589153, 2434767459, -1623178306]);
+        assert_eq!(result.iter().sum::<isize>(), 1623178306);
+    }
+
 }
--- day20/src/lib.rs
+++ day20/src/lib.rs
@@ -1,5 +1,4 @@
 use std::fmt;
-use std::cmp::max;
 
 pub struct Number {
     pub value: isize,
@@ -31,25 +30,83 @@ pub fn move_by_value(list: Vec<isize>) -> Vec<isize> {
             let mut number = list.remove(i);  // Pop
             number.moved = true;
             // Calculate index
-            let mut new_index = i as isize + number.value;
-            if new_index <= 0 || new_index >= len as isize {  // Wrap around left
-                new_index = new_index.rem_euclid(len as isize - 1);
-                if new_index == 0 {  // Needs one more to overflow
-                    new_index = (len-1) as isize;
-                }
+            let mut new_index = (i as isize + number.value).rem_euclid(len as isize - 1);
+
+            if new_index == 0 {  // Needs one more to overflow
+                new_index = (len-1) as isize;
             }
             let new_index = new_index as usize;
 
             list.insert(new_index, number);  // Push
             
-            // if new_index > i {  // If insertion messed up indexing
-                // }
-            if i > 0 {
-                i -= 1;
+            if new_index > i {  // If insertion messed up indexing
+                continue;  // Don't increment `i`
             }
-            continue;  // Don't increment `i`
         }
         i += 1;
+    }
+
+    // println!("FINAL {list:?}");
+
+    // Convert back to plain list
+    list.iter().map(|item| item.value).collect()
+}
+
+/// Number for part B, allowing a number of moves
+pub struct NumberB {
+    pub value: isize,
+    pub moved: usize,
+    pub i: usize,
+}
+impl NumberB {
+    pub fn new(i: usize, value: isize) -> Self {
+        Self { value, moved: 0, i }
+    }
+}
+impl fmt::Debug for NumberB {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}", self.value, self.moved)
+    }
+}
+
+pub fn move_by_value_times(list: Vec<isize>, times: usize) -> Vec<isize> {
+    // Add `moved` attribute to items
+    let mut list: Vec<NumberB> = list.iter().enumerate().map(|(i, item)| NumberB::new(i, *item)).collect();
+    let len = list.len();
+
+    for round in 0..times {
+        let mut i = 0;
+    
+        while i < list.len() {
+            // Make sure to use the same indexes as decided at the start
+            // Really inefficient but it works ¯\_(ツ)_/¯
+            let list_index = list.iter().position(|item| item.i == i).unwrap();
+            let number = &list[list_index];
+
+            if number.moved <= round && number.value != 0 {  // If it needs to be moved
+                // println!("List: {list:?} list[{i}] = {number:?}");
+
+                let mut number = list.remove(list_index);  // Pop
+                number.moved += 1;
+                // Calculate index
+                let mut new_index = (list_index as isize + number.value).rem_euclid(len as isize - 1);
+
+                if new_index == 0 {  // Needs one more to overflow
+                    new_index = (len-1) as isize;
+                }
+                let new_index = new_index as usize;
+
+                list.insert(new_index, number);  // Push
+                
+                if new_index > list_index {  // If insertion messed up indexing
+                    continue;  // Don't increment `i`
+                }
+            }
+            i += 1;
+        }
+
+        // println!("End of round {round}: {list:?}");
+        assert!(list.iter().all(|item| item.moved == round+1 || item.value == 0));
     }
 
     // println!("FINAL {list:?}");
