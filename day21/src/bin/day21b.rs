6e9d6e5281d6433120811aa09268bcfe734cd5c2
Jorian Woltjer <jorianwoltjer@hotmail.com>
2022-12-21 16:33:14 +0100

    day21

--- day21/src/bin/day21b.rs
+++ day21/src/bin/day21b.rs
@@ -1,3 +1,42 @@
+use day21::Monkeys;
+
 fn main() {
-    unimplemented!("day21b");
+    let input = include_str!("../../input.txt");
+    
+    let monkeys: Monkeys = input.parse().unwrap();
+
+    let human_value = monkeys.find_human_value();
+
+    println!("Value: {human_value}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input = "\
+root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32";
+
+        let monkeys: Monkeys = input.parse().unwrap();
+
+        let human_value = monkeys.find_human_value();
+
+        assert_eq!(human_value, 301);
+    }
 }
--- day21/src/lib.rs
+++ day21/src/lib.rs
@@ -36,6 +36,72 @@ impl Monkeys {
             }
         }
     }
+
+    /// Only returns Some(value) if the calculation does not include "humn"
+    pub fn get_value_without_human(&self, monkey: &String) -> Option<isize> {
+        if monkey == "humn" {
+            return None;
+        }
+
+        let job = &self.monkeys.get(monkey)?.job;
+        
+        match job {
+            Job::CONSTANT(value) => Some(*value),
+            Job::OPERATION(name1, operation, name2) => {
+                let value1 = self.get_value_without_human(name1)?;
+                let value2 = self.get_value_without_human(name2)?;
+
+                Some(operation.apply(value1, value2))
+            }
+        }
+    }
+
+    pub fn find_human_value(&self) -> isize {
+        let job = &self.monkeys.get("root").unwrap().job;
+
+        match job {
+            Job::OPERATION(name1, _operation, name2) => {
+                // One of these will be None
+                let value1 = self.get_value_without_human(name1);
+                let value2 = self.get_value_without_human(name2);
+
+                match (value1, value2) {
+                    (None, Some(value)) => self.solve_unknown(name1, value),
+                    (Some(value), None) => self.solve_unknown(name2, value),
+                    (_, _) => panic!("No or multiple unknowns found, should only be one"),
+                }
+            }
+            Job::CONSTANT(_) => panic!("root cannot have a constant value")
+        }
+    }
+
+    pub fn solve_unknown(&self, unknown: &String, prev_value: isize) -> isize {
+        let job = &self.monkeys.get(unknown).unwrap().job;
+
+        match job {
+            Job::OPERATION(name1, operation, name2) => {
+                // One of these will be None
+                let value1 = self.get_value_without_human(name1);
+                let value2 = self.get_value_without_human(name2);
+
+                // Do inverse operation
+                match (value1, operation, value2) {
+                    (None, Operation::ADD, Some(constant)) => self.solve_unknown(name1, prev_value - constant),
+                    (Some(constant), Operation::ADD, None) => self.solve_unknown(name2, prev_value - constant),
+                    (None, Operation::SUBTRACT, Some(constant)) => self.solve_unknown(name1, prev_value + constant),
+                    (Some(constant), Operation::SUBTRACT, None) => self.solve_unknown(name2, constant - prev_value),
+                    (None, Operation::MULTIPLY, Some(constant)) => self.solve_unknown(name1, prev_value / constant),
+                    (Some(constant), Operation::MULTIPLY, None) => self.solve_unknown(name2, prev_value / constant),
+                    (None, Operation::DIVIDE, Some(constant)) => self.solve_unknown(name1, prev_value * constant),
+                    (Some(constant), Operation::DIVIDE, None) => self.solve_unknown(name2, constant / prev_value),
+                    (_, _, _) => panic!("No or multiple unknowns found, should only be one")
+                }
+            }
+            Job::CONSTANT(_value) => {
+                prev_value
+            }
+        }
+    }
 }
 
 #[derive(Debug)]
